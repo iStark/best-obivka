4864afe04b34cfd58c778b523fb508adcf8825d6
Kuretao <114327440+Kuretao@users.noreply.github.com>
2026-06-19 08:00:27 +0300

    feat: new blocks for SEO

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,12 @@ import { Footer } from './sections/Footer'
 import { Hero } from './sections/Hero'
 import { Materials } from './sections/Materials'
 import { Portfolio } from './sections/Portfolio'
+import { PriceFactors } from './sections/PriceFactors'
 import { Process } from './sections/Process'
 import { Promo } from './sections/Promo'
+import { RepairDirections } from './sections/RepairDirections'
 import { Reviews } from './sections/Reviews'
+import { ServiceAreas } from './sections/ServiceAreas'
 import { Services } from './sections/Services'
 
 function App() {
@@ -27,12 +30,15 @@ function App() {
       <main>
         <Hero />
         <Services />
+        <RepairDirections />
         <Estimate />
+        <PriceFactors />
         <Portfolio />
         <Materials />
         <Process />
         <Promo />
         <Reviews />
+        <ServiceAreas />
         <Faq />
         <Contact />
       </main>
